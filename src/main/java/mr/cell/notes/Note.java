3dca295affd3dc36095dd301d9ff2d514a69cc9d
mr-cell <marcel.schally@gmail.com>
2019-08-01 22:07:50 +0200

    enabling prod profile with AWS RDS connection
    and password managed by aws secrets manager

--- src/main/java/mr/cell/notes/Note.java
+++ src/main/java/mr/cell/notes/Note.java
@@ -15,7 +15,7 @@ import javax.persistence.Id;
 @Entity
 public class Note {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String title;
     private String body;
--- src/main/java/mr/cell/notes/NoteNotFoundException.java
+++ src/main/java/mr/cell/notes/NoteNotFoundException.java
@@ -1,8 +1,13 @@
 package mr.cell.notes;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by Marcel_Schally-Kacpr on 7/23/2019.
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoteNotFoundException extends RuntimeException {
 
     public NoteNotFoundException(Integer id) {
